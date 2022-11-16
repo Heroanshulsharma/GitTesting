25edd4bc97d6638c81c20de1fd77f24acfd927f7
Anshul <heroanshulsharma@gmail.com>
2022-11-16 15:42:30 +0530

    Adding to test Git

---
+++ src/test/java/com/anshul/gittesting/GitTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anshul.gittesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
